4ab63ab086671e195a4a131add6afa13d218fe90
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-20 18:54:26 +0700

    buat kolomnya dulay

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import random 
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8,
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+  all_symbols = []
+  for symbol, symbol_count in symbols.items():
+    for _ in range(symbol_count):
+      all_symbols.append(symbol)
+  
+  columns = []
+  for _ in range(cols):
+    column = []
+    current_symbols = all_symbols[:]
+    for _ in range(rows):
+      value = random.choice(current_symbols)
+      current_symbols.remove(value)
+      column.append
+    
+    columns.append(column)
+  return columns
 
 def deposit():
   while True:
@@ -19,14 +49,14 @@ def deposit():
 
 def get_number_of_lines():
   while True:
-    lines = input("Enter the number lines to bet on (1-" + str(MAX_LINES) + "): ")
+    lines = input("Masukan posisi baris yang mau kamu bet (pilih antara 1 hingga " + str(MAX_LINES) + "): ")
 
     if lines.isdigit():
       lines = int(lines)
       if 1 <= lines <= MAX_LINES:
         break
       else:
-        print("bARIS harus sesuai dengan batas")
+        print("Baris harus sesuai dengan batas")
     else:
       print("Masukan angka")
     
