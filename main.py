6c4158bf0f01fa2a01e737a02dd7ecc645e652bf
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-20 18:41:00 +0700

    basic input validation

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
 def deposit():
   while True:
     amount = input("Berapa duit yang mau km deposit? $")
@@ -11,9 +15,50 @@ def deposit():
     else:
       print("Masukan angka")
     
-    return amount
+  return amount
+
+def get_number_of_lines():
+  while True:
+    lines = input("Enter the number lines to bet on (1-" + str(MAX_LINES) + "): ")
+
+    if lines.isdigit():
+      lines = int(lines)
+      if 1 <= lines <= MAX_LINES:
+        break
+      else:
+        print("bARIS harus sesuai dengan batas")
+    else:
+      print("Masukan angka")
+    
+  return lines
+
+def get_bet():
+  while True:
+    bet = input("Berapa duit yang mau km bet pada masing-masing baris? $")
+
+    if bet.isdigit():
+      bet = int(bet)
+      if MIN_BET <= bet <= MAX_BET:
+        break
+      else:
+        print(f"Taruhan harus antara ${MIN_BET} dan ${MAX_BET}")
+    else:
+      print("Masukan angka")
+
+  return bet
 
 def main():
   balance = deposit()
+  lines = get_number_of_lines()
+  while True:
+    bet = get_bet()
+    total_bet = bet * lines
+
+    if total_bet > balance:
+      print(f"Saldo kamu tidak cukup untuk bet {lines} baris dengan taruhan ${bet} setiap baris")
+    else:
+      break
+
+  print(f"Kamu ngebet {lines} baris dengan taruhan ${bet} setiap baris, dan saldo bet kamu sekarang adalah ${total_bet}")
 
 main()
